6e8849689de296894168410bcb8cc4e8c7d15608
黃宇新 <astroboy03@cathaylife.com.tw>
2020-12-28 17:24:39 +0800

    initial project

---
+++ iNDEFExchange/iNDEFExchange/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iNDEFExchange
+//
+//  Created by i9400506 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
